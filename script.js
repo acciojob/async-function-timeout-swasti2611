e3947cb098f9aafc272265a56859c791902d5b1a
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-27 21:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const textInput = document.getElementById('text');
+const delayInput = document.getElementById('delay');
+const outputDiv = document.getElementById('output');
+const submitBtn = document.getElementById('btn');
+
+async function displayTextWithDelay() {
+  const text = textInput.value;
+  const delay = parseInt(delayInput.value);
+
+  if (text && !isNaN(delay)) 
+  {
+    outputDiv.textContent = '';
+    setTimeout(() => {
+      outputDiv.textContent = text;
+    }, delay);
+  }
+}
+
+submitBtn.addEventListener('click', displayTextWithDelay);
